1d1dfd641680f1db477f4b20178abbb233cf6499
vboxuser <vboxuser@WIN10>
2026-02-13 16:27:10 +0300

    Реализовано меню приложения

--- Bookmaster/View/Windows/MainWindow.xaml.cs
+++ Bookmaster/View/Windows/MainWindow.xaml.cs
@@ -23,5 +23,51 @@ namespace Bookmaster.View.Windows
         {
             InitializeComponent();
         }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            if (loginWindow.ShowDialog() == true )
+            {
+                LibraryMi.Visibility = Visibility.Visible;
+                LoginMi.Visibility = Visibility.Collapsed;
+                LogoutMi.Visibility = Visibility.Visible;
+            }
+            
+
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+            //Вызод из системы
+            LibraryMi.Visibility = Visibility.Collapsed;
+            LoginMi.Visibility = Visibility.Visible;
+            LogoutMi.Visibility = Visibility.Collapsed;
+        }
+
+        private void CloseMi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RepostMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BrowsCatztlogMi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
